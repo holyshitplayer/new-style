dd9a108695b243c15230e09fbaaf649cae89ad3b
holyshitplayer <nemirdota@gmail.com>
2023-03-20 15:05:37 +0200

    Fixed 100vh in mobile

--- js/script.js
+++ js/script.js
@@ -66,3 +66,10 @@ tabsKeys.forEach(tabsKey => {
         });
     });
 });
+
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener("resize", () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
